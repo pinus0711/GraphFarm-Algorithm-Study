03fe2f4e74906694ab5af171c9e7cab447930465
sws6641 <sws6641@naver.com>
2024-01-15 16:43:08 +0900

    ✨feat: BOJ_2559_수열, BOJ_11659_구간함구하기4, BOJ_11660_구간합구하기5

---
+++ wonseop/week3/BOJ_11659_구간합구하기4.java
@@ -0,0 +1,31 @@
+package week3;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_11659_구간합구하기4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) + arr[i-1];
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            System.out.println(arr[y] - arr[x-1]);
+        }
+
+    }
+}
